ec331bb2558e3f0ec798c36b3d0743a40499c296
Craig Jones <artistcdmj@gmail.com>
2024-09-21 13:06:54 -0500

    Update UV_Wire_Edit.py
    added to 3d view a panel to adjust the wire color and thickness

--- UV_Wire_Edit.py
+++ UV_Wire_Edit.py
@@ -19,14 +19,41 @@ class UVWireColor(bpy.types.Panel):
         prefs = bpy.context.preferences
         theme = prefs.themes[0].image_editor
         
-        # Display the color picker for UV wire color
-        layout.prop(theme, "wire_edit", text="UV Wire Color")
+        if context.area.ui_type == 'UV':
+            layout.prop(theme, "wire_edit", text="UV Wire in Edit")
+        else:
+            layout.prop(theme, "uv_shadow", text="UV Wire in Paint")
+
+class VIEW3DWireColor(bpy.types.Panel):
+    """Creates a Panel in the 3D View to change the Edit Mode wire color"""
+    bl_label = "3D View Wire Color"
+    bl_idname = "VIEW3D_PT_wire_color"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'View'
+    
+    @classmethod
+    def poll(cls, context):
+        return context.mode == 'EDIT_MESH'
+
+    def draw(self, context):
+        layout = self.layout
+        prefs = bpy.context.preferences
+        theme = prefs.themes[0].view_3d
+        
+        layout.prop(theme, "wire_edit", text="Wire in Edit")
+        layout.prop(theme, "edge_width", text = "Edge Width")
+        
+
 
 def register():
     bpy.utils.register_class(UVWireColor)
+    bpy.utils.register_class(VIEW3DWireColor)
 
 def unregister():
     bpy.utils.unregister_class(UVWireColor)
+    bpy.utils.unregister_class(VIEW3DWireColor)
 
 if __name__ == "__main__":
     register()
+
